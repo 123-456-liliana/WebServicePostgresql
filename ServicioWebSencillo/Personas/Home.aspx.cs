d84d516e2167a01ba88ecfe47d54208177831a9a
LuisA.Tapia <angeltapia2a@gmail.com>
2020-04-01 16:07:25 -0600

    Creación de Servicio Web en C# Conexion PostgreSQL

---
+++ ServicioWebSencillo/Personas/Home.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Personas
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            WebServiceDB.WebService1SoapClient ws = new WebServiceDB.WebService1SoapClient();
+            DataSet ds = ws.GetData();
+            GridView1.DataSource = ds.Tables[0];
+            GridView1.DataBind();
+
+        }
+    }
+}
